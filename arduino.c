cd7e7fc338cedb6927e8774fa94d0ebaa163ec15
jul213 <158296107+jul213@users.noreply.github.com>
2024-12-11 21:53:16 +0000

    creacion de codigo c para hacer secuencia de parpadeo led en arduino

---
+++ arduino.c
@@ -0,0 +1,18 @@
+
+int led1 = 1;
+
+int led2 = 2;
+
+void setup(){
+
+    pinmode(led1, OUTPUT);
+
+    pinmode(led2, OUTPUT);
+
+}
+
+void loop(){
+
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+}
