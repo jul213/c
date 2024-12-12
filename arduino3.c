9eb75b7a8b845f9f4d790f7907dd19e14d382f0b
jul213 <158296107+jul213@users.noreply.github.com>
2024-12-12 11:03:19 +0000

    creacion de arduino3.c

---
+++ arduino3.c
@@ -0,0 +1,25 @@
+
+#include <LiquidCrystal.h>
+
+int pinPulsador = 3;
+
+
+
+const rs=13, en=12, d4=11, d3=10, d2=9, d1=8;
+LiquidCrystal lcd(rs, en, d4, d3, d2, d1);
+
+void setup(){
+
+    pinmode(pinPulsador, INPUT);
+
+    lcd.begin(15,3);
+}
+
+void loop(){
+
+    int state = digitalRead(pinPulsador);
+
+    lcd.setCursor(0,0);
+    
+}
+
