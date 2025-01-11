fe6bb09691619abf5ca46148de723865dc2cd940
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-11 20:40:59 +0000

    IMPLEMENTACION DEL BUZZER

--- mq.c
+++ mq.c
@@ -1,5 +1,5 @@
-int valor;
-
+int VALOR;
+int BUZZER = 2;
 void setup () {
     Serial.begin(9600);
     // entrada analogica no requiere inicializacion
@@ -7,6 +7,10 @@ void setup () {
 
 void loop () {
     VALOR = analogRead(A0);
+    if (VALOR > 400)
+    digitalWrite(BUZZER, HIGH);
+    else
+    digitalWrite(BUZZER, LOW);
     Serial.println(VALOR);
     delay(500);
 }
