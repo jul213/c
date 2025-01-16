01bfa4e54c8bcdc23f0d5761fd1178eb4292d6ff
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-16 21:39:07 +0100

    Create conversor.c

---
+++ conversor.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+float metros_pies (float metros) {
+ return metros * 3.280;
+}
+
+float pies_metros (float pies) {
+  return pies / 3.280;
+}
+
+float kg_libras (float kg){
+ return kg * 2.204;
+}
+
+float libras_kg (float libras){
+  return libras / 2.204;
+}
+
+int main()
