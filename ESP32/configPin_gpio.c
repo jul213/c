90ce73f52e16b42e7a4297f588267b3561362e92
jul213 <158296107+jul213@users.noreply.github.com>
2025-01-30 12:18:11 +0000

    funcion para deteminar el nivel del pin (GPIO) LOW O HIGH

--- ESP32/configPin_gpio.c
+++ ESP32/configPin_gpio.c
@@ -5,3 +5,7 @@ static void configureLed(void){
     gpio_reset_pin(BLINK_GPIO);
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 }
+
+static void blink_led(void){
+        gpio_set_level(BLINK_GPIO, s_led_state) //variable definida como global 
+}
