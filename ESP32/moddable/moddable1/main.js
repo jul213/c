fe5817c99a1ae15429534adf5493d0bd577e8847
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-04 12:08:59 +0000

    iniciamos los pines

---
+++ ESP32/moddable/moddable1/main.js
@@ -0,0 +1,15 @@
+import Timer from "timer";
+import Digital from "pins/digital";
+
+debugger;
+
+let led5 = new Digital({
+    pin: 5,
+    mode: Digital.Output
+});
+
+let led10 = new Digital({
+    pin: 10,
+    mode: Digital.Output
+})
+
