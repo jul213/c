c0c97db6e921c1be7e2258f8114e6305decc13c8
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-04 11:17:09 +0000

    empezamos loop para mostrar la temperatura por el serial

--- ESP32/leer_sensor_hall.c
+++ ESP32/leer_sensor_hall.c
@@ -4,3 +4,9 @@ void setup(){
     Serial.begin(115200);
 }
 
+void loop(){
+    float tempe = 0;
+    tempe = temperatureRead();
+    Serial.print("Temperatura: ");
+    Serial.print(temperatura);
+}
