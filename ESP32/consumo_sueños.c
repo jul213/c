3f17c9f8ac25c0e48b9a030e2dc412cc73aeb6ad
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-04 11:27:21 +0000

    terminamos sueño ligero

--- ESP32/consumo_sueños.c
+++ ESP32/consumo_sueños.c
@@ -5,3 +5,11 @@ void setup(){
     Serial.begin(115200);
     delay(650);
 }
+
+int conta = 0;
+void loop(){
+Serial.println(conta);
+conta++
+esp_sleep_enable_timer_wakeup(1 * 100000);
+esp_light_sleep_start();
+}
