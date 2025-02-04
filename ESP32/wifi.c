45803e3e5f1e0f895bdde96e72e5c29dc7443607
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-04 11:48:00 +0000

    terminamos el wifi.c para tener internet en el esp 32 conectarse a la red

--- ESP32/wifi.c
+++ ESP32/wifi.c
@@ -6,5 +6,12 @@ const char* passphrase = "contraseña";
 void setup(){
     Serial.begin(115200);
     delay(600);
-    
+
+    WiFi.begin(ssid,passphrase);
+    while (WiFi.status() != WL_CONNECTED)
+        delay(550);
+    Serial.print("conectado a la red WIFI: ");
+    Serial.print(ssid);
+    Serial.print("Direccion ip: ")
+    Serial.println(WiFi.localIP());
 }
