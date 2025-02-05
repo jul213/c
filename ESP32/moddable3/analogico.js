3efc3a68c4bee2f579c105cc92967af5d88d2331
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-05 22:18:20 +0000

    terminamos programa para leer entrada analogica y mandar por serial

--- ESP32/moddable3/analogico.js
+++ ESP32/moddable3/analogico.js
@@ -1,2 +1,11 @@
 import Analog from "pins/analog";
 import Timer from "timer";
+
+debugger;
+
+const PIN_ADC = 0;
+
+Timer.repeat(() => {
+    let valor = Analog.read(PIN_ADC);
+    trace(`valor leido ${valor}`);
+}, 700);
