672a27932aac168da5f385fcca253007ff42f3da
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-11 09:25:27 +0000

    creamos el monitor con las opciones wifi y hacemos un switch para segun los dos 3 casos se ejecuten lo apropiado

--- ESP32/moddable7/main.js
+++ ESP32/moddable7/main.js
@@ -2,9 +2,20 @@ import config from "mc/config";
 import WiFi from "wifi";
 import Net from "net";
 import Timer from "timer";
+import { trace } from "console";
 
 const {ssid, password} = config;
 
 WiFi.mode = 1;
 
-trace(`ssid: ${ssid}, pass: ${password}`)
+trace(`ssid: ${ssid}, pass: ${password}`);
+
+const opcionesWifi = {ssid, password};
+
+const monitor = new WiFi(opcionesWifi, msg => {
+    switch (msg) {
+        case WiFi.connected:
+            trace(`conectando a wifi, esperando ip`);
+            break;
+    }
+})
