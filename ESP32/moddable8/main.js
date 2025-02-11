2a86bbd8a3c835c8c0c60930607211078b4171cc
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-11 11:05:10 +0000

    creamos el mqtt(cliente) junto con la mac y el id

--- ESP32/moddable8/main.js
+++ ESP32/moddable8/main.js
@@ -2,3 +2,10 @@ import Net from "net";
 import Client from "mqtt";
 import Timer from "timer";
 
+const mac = Net.get("MAC");
+const id = `ESP32_${mac.replaceAll(":","").slice(-6)}`;
+const mqtt = new Client({
+    host: "10.200.2.10",
+    timeout: 5000,
+    id
+});
