ee748eb99dfd5ab3064c1927ff27f6c73524fa52
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-14 12:30:57 +0000

    creamos constantes y estado del led y boton

--- ESP32/proyecto-esp-IDF/blink.c
+++ ESP32/proyecto-esp-IDF/blink.c
@@ -6,3 +6,11 @@
 #include "esp_log_h"
 #include "sdkconfig.h"
 
+
+static const char *TAG = "Actividad 2 proyecto con esp-idf y freertos";
+
+#define LED_GPIO 5
+#define BUTTON_GPIO 17
+
+static uint8_t s_led_state = 0;
+static uint8_t s_button_state = 0;
