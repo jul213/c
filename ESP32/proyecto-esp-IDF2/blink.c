b16d4428071662d764cdc2ceb96d736eda94701a
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-18 10:51:56 +0000

    configuracion del led1

--- ESP32/proyecto-esp-IDF2/blink.c
+++ ESP32/proyecto-esp-IDF2/blink.c
@@ -18,3 +18,10 @@ static uint32_t uStackDepth = 2048;
 static uint8_t s_led1_state = 0;
 static uint8_t s_led2_state = 0;
 static uint8_t s_led3_state = 0;
+
+
+static void configure_led(void){
+    ESP_LOGI(TAG, "configuracion de led");
+    gpio_reset_pin(led1);
+    gpio_set_direction(led1, GPIO_MODE_OUTPUT);
+}
