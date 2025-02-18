522d6ec5d81278e922a5b1cc3f7a68e22022e046
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-18 10:47:17 +0000

    definicion del estado de 3 leds

--- ESP32/proyecto-esp-IDF2/blink.c
+++ ESP32/proyecto-esp-IDF2/blink.c
@@ -13,4 +13,8 @@ static const char *TAG = "actividad 3 proyecto con esp-idf";
 #define led3 18;
 
 
-static uint32_t uStackDepth = 2048
+static uint32_t uStackDepth = 2048;
+
+static uint8_t s_led1_state = 0;
+static uint8_t s_led2_state = 0;
+static uint8_t s_led3_state = 0;
