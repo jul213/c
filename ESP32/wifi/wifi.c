60091de4b8de9641411973a1a7a4ea91247c9dce
julio <158296107+jul213@users.noreply.github.com>
2025-04-23 23:31:51 +0000

    creacion de acces point del esp32

---
+++ ESP32/wifi/wifi.c
@@ -0,0 +1,50 @@
+#include <U8g2lib.h>
+#include <Wire.h>
+#include <Servo.h>
+#include <ESP8266WiFi.h>
+
+#define SCL 12
+#define SDA 14
+#define SERVO_PIN 5
+
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, 12, 14, U8X8_PIN_NONE);
+Servo myServo;
+
+const char* ssid = "ESP8266-SERVO_MOTOR-AP";
+const char* password = "1234456789";
+
+void setup()
+{
+  Serial.begin(115200);
+
+  WiFi.softAP(ssid, password);
+  Serial.print("Punto de acceso iniciado. IP: ");
+  Serial.println(WiFi.softAPIP());
+
+  u8g2.begin();
+  u8g2.enableUTF8Print();
+  myServo.attach(SERVO_PIN);
+}
+
+void loop() {
+  u8g2.clearBuffer();                    // Clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);   // Choose a font
+  u8g2.drawStr(0, 10, "Hola julio cesar gomez!");   // Write the string to the internal memory
+  u8g2.sendBuffer();                     // Transfer internal memory to the display
+
+  int numClients = WiFi.softAPgetStationNum();
+  Serial.print("Dispositivos conectados: ");
+  Serial.println(numClients);
+
+  for (int i=0; i <= 180; i++){
+    myServo.write(i);
+    delay(15);
+  }
+  delay(1000);                          // Pause for a second
+
+  for (int i = 180; i>= 0; i--){
+    myServo.write(i);
+    delay(15);
+  }
+  delay(1000);
+}
