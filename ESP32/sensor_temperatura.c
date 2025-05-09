8db8fca6e40e2a77a0e8a894edc76a326a2ca640
julio <158296107+jul213@users.noreply.github.com>
2025-05-09 08:28:44 +0000

    terminamos el programa del sensor para que lea temperatura de un lm35

--- ESP32/sensor_temperatura.c
+++ ESP32/sensor_temperatura.c
@@ -1,2 +1,15 @@
 #define sensor_t 2
 
+float voltaje = 0;
+float temperatura = 0;
+
+void setup(){
+    Serial.begin(9600);
+    analogReadResolution(10);
+}
+
+void loop(){
+    voltaje = analogRead(sensor_t) * 3.3/1024;
+    temperatura = voltaje * 100;
+    Serial.print(temperatura);
+}
