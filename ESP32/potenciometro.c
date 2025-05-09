95e3fb7b0f166bdcc9f5c95c045e189c768a51ea
julio <158296107+jul213@users.noreply.github.com>
2025-05-09 07:16:10 +0000

    creamos el loop variables

---
+++ ESP32/potenciometro.c
@@ -0,0 +1,16 @@
+#define pot 29
+#define led 30
+
+int valorPot = 0;
+
+void setup(){
+    pinMode(led, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop(){
+    valorPot = analogRead(pot);
+    Serial.print("valor del potenciometro");
+    Serial.println(valorPot);
+    digitalWrite(led, HIGH);
+}
